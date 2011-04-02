e49cfb7cbc861caad74dc68c009715eed047f0d0
anonymous74100 <anonymous.74100@gmail.com>
2011-04-02 16:20:25 +0300

    createTPL.pl now only creates TPL

--- tools/createTPL.pl
+++ tools/createTPL.pl
@@ -1,6 +1,7 @@
-#!/usr/bin/perl -w
-# Script for the generation of the EasyList subscriptions
+#!/usr/bin/env perl
+
 # Copyright 2011 Wladimir Palant and Michael
+# Adapted by anonymous74100
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,183 +16,23 @@
 # limitations under the License.
 
 use strict;
+use warnings;
 use File::Basename;
-use Digest::MD5 qw(md5_base64);
-use Compress::Zlib;
-use POSIX qw(strftime);
-require Encode;
 
-my $rootdir = dirname($0);
-my $sourcedir = "$rootdir";
-my $subsdir = "$rootdir/subscriptions";
-my %verbatim = map {$_ => 1} qw(COPYING);
+die "Usage: $^X $0 subscription.txt\n" unless @ARGV;
 
-unless (-d "$subsdir")
-{
-  mkdir ($subsdir) || die "$subsdir does not exist and cannot be created.";
-}
+my $file = $ARGV[0];
+my $data = readFile($file);
+my $path = dirname($file);
 
-if (-f "$subsdir/.lock")
-{
-  my $pid = readFile("$subsdir/.lock");
-  exit if kill(0, $pid);
-}
+# Convert ABP list to TPL
+$data = convertToTPL($data);
 
-writeFile("$subsdir/.lock", $$);
+# Generate TPL filename
+my $tplfilename = fileparse("$file", qr/\.[^.]*/).".tpl";
+# Write TPL
+writeFile("$path/$tplfilename",$data);
 
-system("hg", "pull", "--update", $sourcedir);
-
-my %exists = ();
-
-opendir(local *SOURCE, $sourcedir) || die "Could not open directory $sourcedir.";
-foreach my $file (readdir(SOURCE))
-{
-  next unless ($file =~ /\.txt$/ && $file ne "robots.txt") || exists($verbatim{$file});
-
-  my $data = readFile("$sourcedir/$file");
-
-  if (exists($verbatim{$file}))
-  {
-    $exists{$file} = 1;
-    if (-f "$subsdir/$file")
-    {
-      my $cmp = readFile("$subsdir/$file");
-      next if $cmp eq $data;
-    }
-  }
-  else
-  {
-    my ($header) = ($data =~ /^(.*)/);
-    next unless $header =~ /\[Adblock(?:\s*Plus\s*([\d\.]+)?)?\]/i;
-
-    $data = resolveIncludes($data);
-    unless ($data)
-    {
-      warn "Failed to resolve includes in $file.";
-      $exists{$file} = -f "$subsdir/$file";
-      next;
-    }
-
-    $exists{$file} = 1;
-
-    $data =~ s/\r//g;
-    $data =~ s/\n+/\n/g;
-    $data =~ s/^.*!\s*checksum[\s\-:]+([\w\+\/=]+).*\n//gmi;
-
-    if (-f "$subsdir/$file")
-    {
-      my $cmp1 = readFile("$subsdir/$file");
-      $cmp1 =~ s/^.*!\s*checksum[\s\-:]+([\w\+\/=]+).*\n//gmi;
-      $cmp1 =~ s/\s*\d+ \w+ \d+ \d+:\d+ UTC//g;
-
-      my $cmp2 = $data;
-      $cmp2 =~ s/\s*\d+ \w+ \d+ \d+:\d+ UTC//g;
-      next if $cmp1 eq $cmp2;
-    }
-
-    my $checksum = md5_base64($data);
-    $data =~ s/\n/\n! Checksum: $checksum\n/;
-  }
-
-  writeFile("$subsdir/$file", $data);
-
-  unlink("$subsdir/$file.gz");
-  system("7za", "a", "-tgzip", "-mx=9", "-mpass=15", "$subsdir/$file.gz", "$subsdir/$file") && warn "Failed to compress file $subsdir/$file. Please ensure that p7zip is installed on the system.";
-}
-closedir(SOURCE);
-
-opendir(local *TARGET, $subsdir) || die "Could not open directory $subsdir.";
-foreach my $file (readdir(TARGET))
-{
-  next if $file =~ /^\./ || $file !~ /\.txt$/;
-
-  my $data = readFile("$subsdir/$file");
-  $file =~ s/\.txt$/.tpl/;
-  $exists{$file} = 1;
-
-  my $converted = convertToTPL($data);
-  if (-f "$subsdir/$file")
-  {
-    my $cmp = readFile("$subsdir/$file");
-    next if $cmp eq $converted;
-  }
-  writeFile("$subsdir/$file", $converted);
-
-  unlink("$subsdir/$file.gz");
-  system("7za", "a", "-tgzip", "-mx=9", "-mpass=15", "$subsdir/$file.gz", "$subsdir/$file") && warn "Failed to compress file $subsdir/$file. Please ensure that p7zip is installed on the system.";
-}
-
-opendir(local *TARGET, $subsdir) || die "Could not open directory $subsdir.";
-foreach my $file (readdir(TARGET))
-{
-  next if -d "$subsdir/$file" || $file =~ /\.gz$/ || exists($exists{$file});
-
-  unlink("$subsdir/$file");
-  unlink("$subsdir/$file.gz");
-}
-closedir(TARGET);
-
-unlink("$subsdir/.lock");
-
-sub resolveIncludes
-{
-  my ($data, $level) = @_;
-  $level = $level || 0;
-
-  if ($level > 5)
-  {
-    warn "There are too many instances of \%include\% in the data. It is possible that circular reference is present.";
-    return undef;
-  }
-
-  my $failures = 0;
-  $data =~ s/^\s*%include\s+(.*)%\s*$/my $include = resolveInclude($1, $level);$include ? $include : $failures++/gem;
-
-  my $timestamp = strftime("%e %b %Y %R UTC", gmtime);
-  $data =~ s/%timestamp%/$timestamp/g;
-
-  return ($failures ? undef : $data);
-}
-
-sub resolveInclude
-{
-  my ($file, $level) = @_;
-
-  if ($file =~ /^[\w\-\+][\w\.\-\+]*(?:\/[\w\-\+][\w\.\-\+]*)*$/ && -f "$sourcedir/$file")
-  {
-    my $data = readFile("$sourcedir/$file");
-    $data =~ s/\r//g;
-    $data =~ s/^.*?%timestamp%.*?$//gm;
-    $data =~ s/\n+/\n/g;
-
-    my ($header) = ($data =~ /^(.*)/);
-    $data =~ s/^.*\n// if $header =~ /\[Adblock(?:\s*Plus\s*([\d\.]+)?)?\]/i;
-    $data = "! *** $file ***\n" . $data;
-    $data = resolveIncludes($data, $level + 1);
-    return $data;
-  }
-  elsif ($file =~ /^https?:\/\//)
-  {
-    my $param = $file;
-    $param =~ s/\W/\\$&/g;
-    my $data = `wget -q -O - $param`;
-
-    return undef unless $data;
-
-    $data = Compress::Zlib::memGunzip($data) || $data;
-
-    $data =~ s/\r//g;
-    $data =~ s/\n+/\n/g;
-
-    my ($header) = ($data =~ /^(.*)/);
-    return undef unless $header =~ /\[Adblock(?:\s*Plus\s*([\d\.]+)?)?\]/i;
-    $data =~ s/^.*\n//;
-
-    $data = "! *** Fetched from: $file ***\n" . $data;
-    return $data;
-  }
-  return undef;
-}
 
 sub convertToTPL
 {
@@ -201,7 +42,7 @@ sub convertToTPL
 
   #Add necessary definitions
   push @result, "msFilterList";
-  push @result, ": Expires=5";
+  push @result, ": Expires=2";
 
   foreach my $line (split(/\n/, $data))
   {
@@ -224,9 +65,6 @@ sub convertToTPL
     }
     else
     {
-      #Remove lone third party options
-      $line =~ s/\$third-party$//;
-
       #Translate domain blocks
       if ($line =~ /^(|@@)\|\|.*?\^/)
       {
@@ -270,6 +108,11 @@ sub convertToTPL
       {
         $line = "# " . $original;
       }
+      #Comment out lone third party options
+      elsif ($line =~ s/\$third-party$//)
+      {
+        $line = "# " . $original;
+      }
       #Translate all domain filters
       elsif ($line =~ m/^\|\|.*?(^|\/)/)
       {
@@ -296,7 +139,7 @@ sub readFile
 {
   my $file = shift;
 
-  open(local *FILE, "<", $file) || die "Could not read file '$file'.";
+  open(local *FILE, "<", $file) || die "Could not read file '$file'\n";
   binmode(FILE);
   local $/;
   my $result = <FILE>;
@@ -309,7 +152,7 @@ sub writeFile
 {
   my ($file, $contents) = @_;
 
-  open(local *FILE, ">", $file) || die "Could not write file '$file'.";
+  open(local *FILE, ">", $file) || die "Could not write file '$file'\n";
   binmode(FILE);
   print FILE $contents;
   close(FILE);
