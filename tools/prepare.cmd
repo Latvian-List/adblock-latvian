85c5f9af8310eb4aa77ff38d30f039b259f19d1a
anonymous74100 <anonymous.74100@gmail.com>
2011-04-02 18:06:09 +0300

    add urlfilter sorting to preparation batch file

--- tools/prepare.cmd
+++ tools/prepare.cmd
@@ -1,11 +1,13 @@
 :: Prepare Latvian List
 ::
+@cls
 :: Prepare Firefox list
 perl sorter.pl ..\latvian-list.txt
 perl addChecksum.pl ..\latvian-list.txt
 ::
 :: Prepare Opera list
 ::perl automagically_create_opera_filters.pl ..\latvian-list.txt
+perl sorter.pl ..\urlfilter.ini
 perl addChecksum_Opera.pl ..\urlfilter.ini
 perl addChecksum.pl ..\element-filter.css
 ::
