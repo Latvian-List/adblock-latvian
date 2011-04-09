947b83407d69bb7ab3170f9a13857e118131d2a1
anonymous74100 <anonymous.74100@gmail.com>
2011-04-09 17:22:16 +0300

    remove TPL generating script

--- tools/prepare.cmd
+++ tools/prepare.cmd
@@ -6,12 +6,10 @@ perl sorter.pl ..\latvian-list.txt
 perl addChecksum.pl ..\latvian-list.txt
 ::
 :: Prepare Opera list
-::perl createOperaFilters.pl ..\latvian-list.txt
 perl sorter.pl ..\urlfilter.ini
 perl addChecksum_alt.pl ..\urlfilter.ini
 perl addChecksum.pl ..\element-filter.css
 ::
 :: Prepare IE list
-perl createTPL.pl ..\latvian-list.txt
 ::
 pause
--- tools/prepare.sh
+++ tools/prepare.sh
@@ -8,13 +8,11 @@ perl sorter.pl ../latvian-list.txt
 perl addChecksum.pl ../latvian-list.txt
 #
 # Prepare Opera list
-#perl createOperaFilters.pl ../latvian-list.txt
 perl sorter.pl ../urlfilter.ini
 perl addChecksum_alt.pl ../urlfilter.ini
 perl addChecksum.pl ../element-filter.css
 #
 # Prepare IE list
-perl createTPL.pl ../latvian-list.txt
 #
 echo -n "Press enter to proceed:"
 read xx
