66b8803a988426ae60526f8d84aa0bacd53c8498
anonymous74100 <anonymous.74100@gmail.com>
2011-06-09 17:27:54 +0300

    ignore case of characters when comparing filters to whitelists in Opera filter script

--- tools/createOperaFilters.pl
+++ tools/createOperaFilters.pl
@@ -207,7 +207,7 @@ sub createUrlfilter
     }
     foreach my $inline (split(/\n/, $whitelists))
     {
-      $matcheswhitelist = 1 if (($tmpline =~ m/\Q$inline\E/) or ($inline =~ m/\Q$tmpline\E/));
+      $matcheswhitelist = 1 if (($tmpline =~ m/\Q$inline\E/i) or ($inline =~ m/\Q$tmpline\E/i));
     }
 
     if (!defined($matcheswhitelist))
