a08f3e701148e002a9908ad01e90eb5816ab644e
anonymous74100 <anonymous.74100@gmail.com>
2011-06-19 09:21:26 +0300

    modified prepare scripts to match new paths

--- prepare.sh
+++ prepare.sh
@@ -4,17 +4,17 @@
 #
 clear
 # Prepare Firefox list
-perl sorter.pl ../latvian-list.txt
-perl addChecksum.pl ../latvian-list.txt
+perl tools/sorter.pl lists/latvian-list.txt
+perl tools/addChecksum.pl lists/latvian-list.txt
 #
 # Prepare Opera list
-perl createOperaFilters.pl ../latvian-list.txt -nocss
-perl addChecksum_alt.pl ../urlfilter.ini
-perl addChecksum.pl ../element-filter.css
+perl tools/createOperaFilters.pl lists/latvian-list.txt -nocss
+perl tools/addChecksum_alt.pl lists/urlfilter.ini
+perl tools/addChecksum.pl lists/element-filter.css
 #
 # Prepare IE list
-perl createIETPL.pl ../latvian-list.txt
-perl addChecksum_alt.pl ../latvian-list.tpl
+perl tools/createIETPL.pl lists/latvian-list.txt
+perl tools/addChecksum_alt.pl lists/latvian-list.tpl
 #
 echo -n "Press enter to proceed:"
 read xx
