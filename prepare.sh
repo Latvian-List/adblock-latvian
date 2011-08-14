ec54a5eca109da5819c1637817d35a5915f2bac1
anonymous74100 <anonymous.74100@gmail.com>
2011-08-14 14:47:56 +0300

    fixed shell script

--- prepare.sh
+++ prepare.sh
@@ -9,12 +9,12 @@ perl tools/addChecksum.pl lists/latvian-list.txt
 #
 # Prepare Opera list
 perl tools/createOperaFilters.pl lists/latvian-list.txt --nocss
-perl tools/addChecksum_alt.pl lists/urlfilter.ini
+perl tools/addChecksum_Opera.pl lists/urlfilter.ini
 perl tools/addChecksum.pl lists/element-filter.css
 #
 # Prepare IE list
 perl tools/createIETPL.pl lists/latvian-list.txt
-perl tools/addChecksum_alt.pl lists/latvian-list.tpl
+perl tools/addChecksum_IE.pl lists/latvian-list.tpl
 #
 echo -n "Press enter to proceed:"
 read xx
