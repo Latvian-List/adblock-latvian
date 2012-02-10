7c443237d7411a2f56282e4bd17aebf1ba5f94af
anonymous74100 <anonymous.74100@gmail.com>
2012-02-10 10:43:13 +0200

    removed useless references

--- tools/addChecksum_Opera.pl
+++ tools/addChecksum_Opera.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/env perl
 
 # Copyright 2011 Wladimir Palant
-# Adapted by anonymous74100
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
