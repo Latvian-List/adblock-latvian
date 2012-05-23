6b6f7fa1cdbec7e96bf1124ff7446d25b543d32b
anonymous74100 <anonymous.74100@gmail.com>
2012-05-23 17:24:58 +0300

    use opera filter script custom CSS feature

--- prepare.sh
+++ prepare.sh
@@ -8,7 +8,7 @@ perl tools/sorter.pl lists/latvian-list.txt
 perl tools/addChecksum.pl lists/latvian-list.txt
 #
 # Prepare Opera list
-perl tools/createOperaFilters.pl lists/latvian-list.txt --nocss
+perl tools/createOperaFilters.pl lists/latvian-list.txt --addcustomcss lists\specific_elements.css
 perl tools/addChecksum_Opera.pl lists/urlfilter.ini
 perl tools/addChecksum.pl lists/element-filter.css
 #
