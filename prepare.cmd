bf95a250f92e28610988ac303cd87f230a128b60
anonymous74100 <anonymous.74100@gmail.com>
2013-05-24 19:39:43 +0300

    remove Opera urlfilter.ini and element hiding CSS

--- prepare.cmd
+++ prepare.cmd
@@ -5,9 +5,4 @@
 perl tools\sorter.pl lists\latvian-list.txt
 perl tools\addChecksum.pl lists\latvian-list.txt
 ::
-:: Prepare Opera list
-perl tools\createOperaFilters.pl lists\latvian-list.txt --addcustomcss lists\specific_elements.css
-perl tools\addChecksum.pl lists\urlfilter.ini --opera
-perl tools\addChecksum.pl lists\element-filter.css
-::
 pause
--- prepare.sh
+++ prepare.sh
@@ -7,10 +7,5 @@ clear
 perl tools/sorter.pl lists/latvian-list.txt
 perl tools/addChecksum.pl lists/latvian-list.txt
 #
-# Prepare Opera list
-perl tools/createOperaFilters.pl lists/latvian-list.txt --addcustomcss lists\specific_elements.css
-perl tools/addChecksum.pl lists/urlfilter.ini --opera
-perl tools/addChecksum.pl lists/element-filter.css
-#
 echo -n "Press enter to proceed:"
 read xx
