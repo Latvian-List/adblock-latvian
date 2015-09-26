88664713eb2340a5f566a277f0fa1ee5ee359a11
anonymous74100 <anonymous.74100@gmail.com>
2015-09-26 20:57:18 +0300

    prepare.sh: small adjustments

--- prepare.sh
+++ prepare.sh
@@ -2,10 +2,6 @@
 
 # Prepare Latvian List
 #
-clear
 # Prepare Firefox list
 perl tools/sorter.pl lists/latvian-list.txt
 perl tools/addChecksum.pl lists/latvian-list.txt
-#
-echo -n "Press enter to proceed:"
-read xx
